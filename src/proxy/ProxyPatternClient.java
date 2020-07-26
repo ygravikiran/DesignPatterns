985ba2bfe39cb72b1b2ed52de937b99bc232551c
ygravikiran <yg.ravikiran@gmail.com>
2020-07-26 18:28:38 +0530

    few others

---
+++ src/proxy/ProxyPatternClient.java
@@ -0,0 +1,8 @@
+package proxy;
+
+public class ProxyPatternClient {
+	public static void main(String[] args) {
+		OfficeInternetAccess access = new ProxyInternetAccess("Ashwani Rajput");
+		access.grantInternetAccess();
+	}
+}
