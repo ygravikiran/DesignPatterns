628a8222c65899cafdf09dcaefc8c8b07fce9809
ygravikiran <yg.ravikiran@gmail.com>
2020-07-26 22:10:01 +0530

    behavioural patterns

---
+++ src/template/Game.java
@@ -0,0 +1,23 @@
+package template;
+
+//This is an abstract class.  
+public abstract class Game {
+
+	abstract void initialize();
+
+	abstract void start();
+
+	abstract void end();
+
+	public final void play() {
+
+		// initialize the game
+		initialize();
+
+		// start game
+		start();
+
+		// end game
+		end();
+	}
+}// End of the Game abstract class.
